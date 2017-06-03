530039681b2ec105ec900e7c8dc56580675e2eb0
Sean Landry <sean.d.landry@gmail.com>
2017-06-03 13:42:23 -0400

    debuged new version

--- s3up.py
+++ s3up.py
@@ -11,12 +11,12 @@ Metadata notes
 - currently these values are hard coded
 
 Usage:
-    s3up <localdir> <s3key> [--metadata METADATA]
+    s3up <localdir> <s3path> [--metadata METADATA]
     s3up -h | --help 
 
 Options: 
     <localdir>               local directory file path
-    <s3key>                  s3 key, e.g. cst-compbio-research-00-buc/
+    <s3path>                  s3 key, e.g. cst-compbio-research-00-buc/
     --metadata METADATA      metadata in json format '{"uid":"6812", "gid":"6812", "mode":"33204"}'
     -h --help                show this screen.
 """ 
@@ -27,6 +27,144 @@ import os
 import sys
 import json
 
+class SmartSync():
+
+    def __init__(self, local = None, s3path = None, meta = None):
+        self.local = local
+        self.s3path = s3path
+        self.bucket = s3path.split('/', 1)[0]
+        self.key = self.parse_prefix(s3path)
+        self.localToKeys = self.find_dirs(local)
+        self.metadir, self.metafile = self.parse_meta(meta)
+
+
+    def parse_meta(self, meta = None):
+        """
+        Parses passed json argument and adds hardcoded mode for files
+        and directories.
+        
+        Args:
+            meta (str): json metadata.
+
+        Returns:
+            metadirjs, metafilejs (json): json string specific for dirs and 
+                                          files.
+
+        """
+        metadir = json.loads(options['--metadata'])
+        metadir["mode"] = "509"
+        metadirjs = json.dumps(metadir)
+        metafile = json.loads(options['--metadata'])
+        metafile["mode"] = "33204"
+        metafilejs = json.dumps(metafile)
+        return metadirjs, metafilejs
+
+    def parse_prefix(self, path = None):
+        """
+        Parse an s3 prefix key path.
+
+        Args:
+            path (str): entire s3 path.
+
+        Returns:
+            path (str): path withouth bucket name.
+
+        """
+        if len(path.split('/', 1)) > 1:
+            return path.split('/', 1)[1]
+        else:
+            return path
+
+    def find_dirs(self, local = None):
+        """
+        Execute find and sort subprocceses to identify and sort all local 
+        child directories of the local argument.  All directories identified
+        are converted to s3 keys.
+
+        Args:
+            local (str): full path to local directory.
+
+        Returns:
+            (list): s3 keys.
+
+        """
+        ## find local directories and sort
+        d = subprocess.Popen(["find", local, "-type", "d"],
+                         stdout = subprocess.PIPE, shell = False)
+
+        dsort = subprocess.Popen(["sort", "-n"], stdin = d.stdout, shell = False,
+                             stdout = subprocess.PIPE)
+
+        ## sorted directories as a list
+        dLst = dsort.communicate()[0].decode().strip().split('\n')
+        return [self.key + k[len(self.local) + 1:] + '/' for k in dLst]
+        
+
+
+    def key_exists(self, key = None):
+         return subprocess.Popen(["aws", "s3api", "head-object", "--bucket",
+                        self.bucket, "--key", key],
+                        stdout = subprocess.PIPE, shell = False)
+
+    def meta_check(self, obj_head = None):
+        meta = json.loads(obj_head.communicate()[0].decode())['Metadata']
+
+        if len(meta) == 0:
+            return False
+        else:
+            return True
+
+    def meta_update(self, key = None, metadata = None):
+        subprocess.run(["aws", "s3api", "copy-object", "--bucket",
+                    self.bucket, "--key", key, "--copy-source",
+                    self.bucket + "/" + key, "--metadata", metadata,
+                    "--metadata-directive", "REPLACE"])
+
+    def create_key(self, key = None, metadata = None): 
+        subprocess.run(["aws", "s3api", "put-object", "--bucket", self.bucket,
+                            "--key", key, "--metadata", metadata])
+
+    def verify_keys(self, keys = None, meta = None):
+
+        for k in keys:
+            try:
+                check = self.key_exists(key = k)
+
+                ## if key does exist check for metadata
+                metaresult = self.meta_check(obj_head = check)
+
+                if not metaresult:
+                    ## if no metadata then add some now
+                    sys.stderr.write('no metadata found for ' + k + ' updating...\n')
+                    update = self.meta_update(key = k, metadata = meta)
+
+            except:
+                ## key does not exist so lets create it
+                sys.stderr.write("creating key '" + k + "'\n")
+
+                self.create_key(key = k, metadata = meta)
+
+                ## debug
+                #check = subprocess.Popen(["aws", "s3api", "head-object", "--bucket", 
+                #                          self.bucket, "--key", k], 
+                #                          stdout = subprocess.PIPE, shell = False)
+
+                #print(json.loads(check.communicate()[0].decode())['Metadata'])
+
+
+
+    def smart_sync(self):
+        ## verify the s3path passed as command line arg
+        self.verify_keys(keys = [self.key], meta = self.metadir)
+
+        ## verify local dirs converted to s3keys
+        self.verify_keys(keys = self.localToKeys[1:], meta = self.metadir)
+
+        ## complete sync
+        s3url = 's3://' + self.s3path
+        subprocess.run(["aws", "s3", "sync", self.local, s3url, "--metadata", self.metafile])
+        
+
 if __name__== "__main__":
     """
     Command line arguments.
@@ -34,84 +172,6 @@ if __name__== "__main__":
 
     options = docopt(__doc__)
 
-    ## parse some args
-    my_bucket = options['<s3key>'].split('/', 1)[0]
-    local = options['<localdir>']
-    metadir = json.loads(options['--metadata'])
-    metadir["mode"] = "509"
-    metadirjs = json.dumps(metadir)
-    metafile = json.loads(options['--metadata'])
-    metafile["mode"] = "33204"
-    metafilejs = json.dumps(metafile)
+    s3_sync = SmartSync(local = options['<localdir>'], s3path = options['<s3path>'], meta = options['--metadata'])
 
-
-    if len(options['<s3key>'].split('/', 1)) > 1:
-        key = options['<s3key>'].split('/', 1)[1]
-    else:
-        key = options['<s3key>']
-
-    sys.stderr.write("bucket: " + my_bucket + " key: " + key + "\n") 
-
-    ## find local directories and sort
-    d = subprocess.Popen(["find", local, "-type", "d"], 
-                         stdout = subprocess.PIPE, shell = False)
-
-    f = subprocess.Popen(["find", local, "-type", "f"], 
-                         stdout = subprocess.PIPE, shell = False)
-    
-    dsort = subprocess.Popen(["sort", "-n"], stdin = d.stdout, shell = False, 
-                             stdout = subprocess.PIPE)
-    
-    fsort = subprocess.Popen(["sort", "-n"], stdin = f.stdout, shell = False, 
-                             stdout = subprocess.PIPE)
-    
-    ## sorted directories and files as lists
-    d_keys = dsort.communicate()[0].decode().strip().split('\n')
-    f_keys = fsort.communicate()[0].decode().strip().split('\n')
-     
-    ## check if s3key exists and has metadata if not create it
-    try:
-        check = subprocess.Popen(["aws", "s3api", "head-object", "--bucket", 
-                                 my_bucket, "--key", key], 
-                                 stdout = subprocess.PIPE, shell = False)
-        
-        ## if s3key does exist but has no metadata then update it now
-        meta = json.loads(check.communicate()[0].decode())['Metadata']
-        
-        if len(meta) == 0:
-            sys.stderr.write('no metadata found for ' + key + ' updating...\n')
-            
-            subprocess.run(["aws", "s3api", "copy-object", "--bucket", 
-                           my_bucket, "--key", key, "--copy-source", 
-                           my_bucket + "/" + key, "--metadata", metadirjs, 
-                           "--metadata-directive", "REPLACE"])
-    
-    except:
-        sys.stderr.write('creating key now...\n')
-       
-        subprocess.run(["aws", "s3api", "put-object", "--bucket", my_bucket, 
-                        "--key", key, "--metadata", metadirjs])
-         
-        ## debug
-        #check = subprocess.Popen(["aws", "s3api", "head-object", "--bucket", 
-        #                          my_bucket, "--key", key], 
-        #                          stdout = subprocess.PIPE, shell = False)
-        
-        #print(json.loads(check.communicate()[0].decode())['Metadata'])
-        
-    ## skip first key in list becuase it is the command line arg
-    for k in d_keys[1:]: 
-        nextK = key + k[len(local) + 1:] + '/'
-        sys.stderr.write("creating folder... '" + nextK + "'\n")
-
-        subprocess.run(["aws", "s3api", "put-object", "--bucket", my_bucket, 
-                        "--key", nextK, "--metadata", metadirjs])
-    
-    for f in f_keys:
-        nextF = key + f[len(local) + 1:]
-        sys.stderr.write("uploading '" + f + "'\n")
-        sys.stderr.write("s3 key: '" + nextF + "'\n")
-        
-        subprocess.run(["aws", "s3api", "put-object", "--bucket", my_bucket, 
-                        "--key", nextF, "--body", f, "--metadata", metafilejs])
-
+    s3_sync.smart_sync()
